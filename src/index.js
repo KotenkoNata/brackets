f623cfecf40227cb2c2ef46454fb6b9555517738
KotenkoNata <kotenkonata8@gmail.com>
2022-01-25 10:33:47 +0200

    feat: add solving for task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+function check(str, bracketsConfig) {
+  const stack = [];
+  const closedBrackets = {};
+  bracketsConfig.forEach(([open, closed]) => closedBrackets[closed] = open)
+  const openBrackets = Object.values(closedBrackets);
+
+  let brackets = [
+    ...Object.keys(closedBrackets),
+    ...Object.values(closedBrackets),
+  ]
+  brackets = new Set(brackets)
+  brackets = [...brackets]
+
+  for (let i = 0; i < str.length; i++) {
+    const current = str[i];
+
+    const isOpen = openBrackets.includes(current)
+    const mapped = closedBrackets[current];
+
+    if (mapped && stack.length > 0) {
+      const top = stack.pop()
+      if (top !== mapped) {
+        if (isOpen) {
+          stack.push(top)
+          stack.push(current)
+          continue;
+        }
+        return false;
+      }
+    } else if (brackets.includes(current)) {
+      stack.push(current);
+    }
+  }
+
+  return stack.length === 0;
 }
+
+module.exports = check;
